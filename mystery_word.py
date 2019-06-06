03a02299d1d3316b39df68163787c6b7fd8cf5d2
Andy Dearing <andy.dearing@gmail.com>
2019-06-06 13:18:22 -0400

    working mode select

---
+++ mystery_word.py
@@ -0,0 +1,29 @@
+
+# function to start game
+def start_game():
+    """Starts the game by asking user for the desired mode and returns
+    an int for which mode they select"""
+    print("")
+    print("Welcome to MYSTERY WORD")
+    print("Select mode:")
+    print("")
+    print("1. Easy (4-6 letters)")
+    print("2. Normal (6-8 letters")
+    print("3. Hard (8+ letters)")
+    print("4. Evil Mode")
+    print("5. Quit")
+    while True:
+        mode_choice = int(input("Choose (1-4) "))
+        if mode_choice in range(1,6):
+            return mode_choice
+        else:
+            print("Invalid Entry, try again")
+print(start_game())
+
+# initialize the game
+with open("words.txt", "r") as word_file:
+    wordlist = word_file.readlines()
+
+# play the game
+
+# choose the word
